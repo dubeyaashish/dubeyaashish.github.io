27ccc4c93f1c6ad878817a98f8429ccd66e7c3ff
dubeyaashish <72406443+dubeyaashish@users.noreply.github.com>
2022-12-14 21:07:03 +0700

    Init

--- cart.js
+++ cart.js
@@ -1,3 +1,4 @@
+// define data in JSON array
 var products = [
     {
         name: "Samsung TV 50",
@@ -24,29 +25,62 @@ var products = [
 // This function will pick the value from the <selet>
 // and add to the table
 function addToCart() {
-    let elProdct = document.getElementById("products")
-    let pVal = elProdct.value
-    console.log(pVal)
+
     let productObj = {
-        name: pVal,
-        quantity: 1,
-        ppu: 1
+        name: $('#products').val(),
+        quantity: $('#qty').val(),
+        ppu: $('#ppu').val()
     }
     
 
     // Clear existing items in the table
-    let productList = document.getElementById("productList")
-    for (let x = 0; x < products.length; x++) {
-        productList.deleteRow(1)
-    }
+    // let productList = document.getElementById("productList")
+    // for (let x = 0; x < products.length; x++) {
+    //     productList.deleteRow(1)
+    // }
+    $('#productBody').html("")
 
     products.push(productObj)
     loadData()
 }
 
 
-function loadData() {
+// TODO Should use product ID instead of name
+function deleteProduct(index) {
+    console.log("DELETE",index)
+    delete products[index]  // delete the element from array
+    $('#productBody').html("")
+    loadData()
+}
 
+function loadData() {
+    let allRows = ""
+    let gross = 0
+    for (let p in products) {
+        let cellName = `<td><img class='icon' src='sign-delete-icon.png' onclick='deleteProduct("${p}")'> ` + products[p].name + "</td>"
+        let cellQuantity = '<td class="text-right">' + products[p].quantity + "</td>"
+        let cellPPU = '<td class="text-right">' + products[p].ppu + "</td>"
+        let total = products[p].ppu * products[p].quantity
+        gross += total
+        let cellTotal = '<td class="text-right">' + total + "</td>"
+        let row = `<tr>${cellName}${cellQuantity}${cellPPU}${cellTotal}</tr>`
+        allRows += row
+    }
+    $('#productBody').html(allRows)
+
+    $("#gross").html(gross)
+
+    let vat = gross * 0.07
+    let net = gross + vat
+    $("#vat").html(vat.toFixed(2))
+    $("#net").html(net.toFixed(2))
+
+}
+
+function loadDataOld() {
+
+    // $('#productBody').html('<tr><td>xxx</td><td>xxx</td><td>xxx</td><td>xxx</td></tr>')
+    
     let productList = document.getElementById("productList")
     let gross = 0
     for (let p in products) {
